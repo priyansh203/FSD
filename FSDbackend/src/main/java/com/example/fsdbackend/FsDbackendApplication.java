c22c8e2e569004a1bdb8b06f4259d80a8368cd61
Priyansh Dobariya <priyansh@Priyanshs-MacBook-Air.local>
2024-02-14 00:23:26 +0530

    first commit

---
+++ FSDbackend/src/main/java/com/example/fsdbackend/FsDbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fsdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsDbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FsDbackendApplication.class, args);
+    }
+
+}
---
+++ FSDbackend/src/test/java/com/example/fsdbackend/FsDbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fsdbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsDbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
