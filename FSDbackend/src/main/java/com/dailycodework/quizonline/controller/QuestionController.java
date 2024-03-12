73f7cffe49e87beba621f37d86fcc29cbbce8085
Priyansh Dobariya <priyansh@Priyanshs-MacBook-Air.local>
2024-03-12 18:06:08 +0530

    backend changes controller file

--- FSDbackend/src/main/java/com/dailycodework/quizonline/controller/QuestionController.java
+++ FSDbackend/src/main/java/com/dailycodework/quizonline/controller/QuestionController.java
@@ -28,33 +28,19 @@ import static org.springframework.http.HttpStatus.CREATED;
 public class QuestionController {
     private final IQuestionService questionService;
 
-    /**
-     * Endpoint to create a new question.
-     * @param question The question object to be created.
-     * @return ResponseEntity containing the created question.
-     */
+
     @PostMapping("/create-new-question")
     public ResponseEntity<Question> createQuestion(@Valid @RequestBody Question question){
         Question createdQuestion = questionService.createQuestion(question);
         return ResponseEntity.status(CREATED).body(createdQuestion);
     }
 
-    /**
-     * Endpoint to get all questions.
-     * @return ResponseEntity containing a list of all questions.
-     */
+
     @GetMapping("/all-questions")
     public ResponseEntity<List<Question>> getAllQuestions(){
         List<Question> questions = questionService.getAllQuestions();
         return ResponseEntity.ok(questions);
     }
-
-    /**
-     * Endpoint to get a question by its ID.
-     * @param id The ID of the question to retrieve.
-     * @return ResponseEntity containing the question.
-     * @throws ChangeSetPersister.NotFoundException if the question is not found.
-     */
     @GetMapping("/question/{id}")
     public ResponseEntity<Question> getQuestionById(@PathVariable Long id) throws ChangeSetPersister.NotFoundException {
         Optional<Question> theQuestion = questionService.getQuestionById(id);
@@ -65,13 +51,6 @@ public class QuestionController {
         }
     }
 
-    /**
-     * Endpoint to update a question.
-     * @param id The ID of the question to update.
-     * @param question The updated question object.
-     * @return ResponseEntity containing the updated question.
-     * @throws ChangeSetPersister.NotFoundException if the question is not found.
-     */
     @PutMapping("/question/{id}/update")
     public ResponseEntity<Question> updateQuestion(
             @PathVariable Long id, @RequestBody Question question) throws ChangeSetPersister.NotFoundException {
@@ -79,33 +58,18 @@ public class QuestionController {
         return ResponseEntity.ok(updatedQuestion);
     }
 
-    /**
-     * Endpoint to delete a question.
-     * @param id The ID of the question to delete.
-     * @return ResponseEntity indicating the success of the operation.
-     */
     @DeleteMapping("/question/{id}/delete")
     public ResponseEntity<Void> deleteQuestion(@PathVariable Long id){
         questionService.deleteQuestion(id);
         return ResponseEntity.noContent().build();
     }
 
-    /**
-     * Endpoint to get all subjects.
-     * @return ResponseEntity containing a list of all subjects.
-     */
     @GetMapping("/subjects")
     public ResponseEntity<List<String>> getAllSubjects(){
         List<String> subjects = questionService.getAllSubjects();
         return ResponseEntity.ok(subjects);
     }
 
-    /**
-     * Endpoint to fetch random questions for a user.
-     * @param numOfQuestions The number of questions to fetch.
-     * @param subject The subject of the questions.
-     * @return ResponseEntity containing a list of random questions.
-     */
     @GetMapping("/quiz/fetch-questions-for-user")
     public ResponseEntity<List<Question>> getQuestionsForUser(
             @RequestParam Integer numOfQuestions, @RequestParam String subject) {
